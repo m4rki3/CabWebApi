ddaf72a37e32a9d1c05de6ab351b3372fdbe59dd
makov_6igpuye <makovich.i@mail.ru>
2023-11-02 21:04:18 +0300

    feature(ui models) add CarModel, OrderModel, DriverRegistrationModel change regex, ErrorMessages in LoginModel, UserRegistrationModel

---
+++ CabWebApi/Models/CarModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CabWebApi.Models;
+public class CarModel
+{
+	[Required(ErrorMessage = "This field is required")]
+	[StringLength(maximumLength: 82, ErrorMessage = "Model name is too long")]
+	[RegularExpression("[A-Za-zА-Яа-я0-9 -]*")]
+	public string ModelName { get; set; }
+
+	[Required(ErrorMessage = "This field is required")]
+	[StringLength(maximumLength: 6, MinimumLength = 6)]
+	[RegularExpression("[А-Яа-я]{1}[0-9]{3}[А-Яа-я]{2}",
+		ErrorMessage = "Series or registration number are incorrect")]
+	public string SeriesRegistrationNumber { get; set; }
+
+	[Required(ErrorMessage = "This field is required")]
+	[StringLength(maximumLength: 3, MinimumLength = 2,
+		ErrorMessage = "Region code must contain 2 or 3 numbers")]
+
+	[RegularExpression("[0-9]*", ErrorMessage = "Region code contains incorrect symbols")]
+	public int RegionCode { get; set; }
+
+	[Required(ErrorMessage = "This field is required")]
+	[RegularExpression("[0-9]*", ErrorMessage = "Driver id contains incorrect symbols")]
+	public int DriverId { get; set; }
+}
---
+++ CabWebApi/Models/OrderModel.cs
@@ -0,0 +1,24 @@
+﻿using CabWebApi.Domain.Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace CabWebApi.Models;
+public class OrderModel
+{
+	[Required(ErrorMessage = "This field is required")]
+	[RegularExpression("[0-9]*", ErrorMessage = "User id contains incorrect symbols")]
+	public int UserId { get; set; }
+
+	[Required(ErrorMessage = "This field is required")]
+	[RegularExpression("[0-9]*", ErrorMessage = "Car id contains incorrect symbols")]
+	public int CarId { get; set; }
+
+	[Required(ErrorMessage = "This object is required")]
+	public Location Departure { get; set; } = null!;
+
+	[Required(ErrorMessage = "This object is required")]
+	public Location Destination { get; set; } = null!;
+
+	[Required(ErrorMessage = "This field is required")]
+	[RegularExpression("[0-9]*", ErrorMessage = "Price contains incorrect symbols")]
+	public int Price { get; set; }
+}
