5a3145d36254357c2a3a857395fda1f719148cbd
makov_6igpuye <makovich.i@mail.ru>
2023-11-03 15:25:19 +0300

    feature(app) add new services, remove authorization add conneciton string to appsettings

--- CabWebApi/Program.cs
+++ CabWebApi/Program.cs
@@ -4,35 +4,56 @@ using CabWebApi.Domain.Interfaces;
 using CabWebApi.Infrastructure.Business;
 using CabWebApi.Infrastructure.Data;
 using CabWebApi.Services.Interfaces;
+using CarWebApi.Infrastructure.Business;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Console;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder();
 builder.WebHost.ConfigureServices(services =>
 {
-    services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options => options.LoginPath = "/login");
-    services.AddAuthorization();
+    services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+            //.AddCookie(options =>
+            //{
+            //    options.LoginPath = "/login";
+            //    options.AccessDeniedPath = "/accessdenied";
+            //    options.LogoutPath = "/logout";
+            //    options.Cookie.Expiration = TimeSpan.FromDays(10);
+            //});
+
     services.AddControllers();
-    services.AddTransient<IModelRepository<User>, ModelRepository<User>>()
-            .AddTransient<IUserService, UserService>()
-            .AddDbContext<DbContext, ApplicationContext>(options =>
-                 options.UseSqlServer(
-                     "Server: (localdb)\\MSSQLLocalDB;" +
-                     "Database: Cab;" +
-                     "Trusted_Connection: true;")
-             );
+
+    services.AddScoped<IModelRepository<User>, ModelRepository<User>>()
+            .AddScoped<IModelRepository<Driver>, ModelRepository<Driver>>()
+            .AddScoped<IModelRepository<Car>, ModelRepository<Car>>()
+            .AddScoped<IModelRepository<Order>, ModelRepository<Order>>()
+            .AddScoped<IModelRepository<Location>, ModelRepository<Location>>();
+
+    services.AddTransient<IModelService<User>, AccountService<User>>()
+            .AddTransient<IModelService<Driver>, AccountService<Driver>>()
+            .AddTransient<IModelService<Car>, CarService>()
+            .AddTransient<IModelService<Location>, LocationService>()
+            .AddTransient<ICarService, CarService>()
+            .AddTransient<IOrderService, OrderService>()
+            .AddTransient<IAccountService<User>, AccountService<User>>()
+            .AddTransient<IAccountService<Driver>, AccountService<Driver>>();
+
+    string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+    services.AddDbContext<DbContext, CabContext>(options =>
+        options.UseSqlServer(connection));
 });
-//builder.Logging.AddConsole();
-//builder.Logging.SetMinimumLevel(LogLevel.Information);
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseStatusCodePages();
 app.UseRouting();
-app.MapControllers();
+app.UseAuthentication();
+//app.UseAuthorization();
+
+//var rewriteOptions = new RewriteOptions().AddRedirect("(.*)/$", "$1");
+//app.UseRewriter(rewriteOptions);
+
 app.UseConsoleLogging<Program>(LogLevel.Information);
+app.MapControllers();
 app.Run();
