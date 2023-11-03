87113562feaae6112bd1b2efcf86429bd5091a94
makov_6igpuye <makovich.i@mail.ru>
2023-11-03 15:15:58 +0300

    feature(builders) add model builders

---
+++ CabWebApi/Content/Builders/OrderBuilder.cs
@@ -0,0 +1,14 @@
+﻿using CabWebApi.Domain.Core;
+
+namespace CabWebApi.Content.Builders;
+public class OrderBuilder
+{
+	protected readonly Order order;
+    public OrderLocationBuilder Route => new OrderLocationBuilder();
+    public OrderConfigBuilder MakeWith => new OrderConfigBuilder();
+    public OrderBuilder()
+    {
+        order = new();
+    }
+    public Order Build() => order;
+}
---
+++ CabWebApi/Content/Builders/OrderConfigBuilder.cs
@@ -0,0 +1,26 @@
+﻿using CabWebApi.Domain.Core;
+
+namespace CabWebApi.Content.Builders;
+public class OrderConfigBuilder : OrderBuilder
+{
+	public OrderConfigBuilder User(int userId)
+	{
+		order.UserId = userId;
+		return this;
+	}
+	public OrderConfigBuilder Car(int carId)
+	{
+		order.CarId = carId;
+		return this;
+	}
+	public OrderConfigBuilder Price(int price)
+	{
+		order.Price = price;
+		return this;
+	}
+	public OrderConfigBuilder Status(OrderStatus status)
+	{
+		order.Status = status;
+		return this;
+	}
+}
