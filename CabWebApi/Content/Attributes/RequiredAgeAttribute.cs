5a63e80517ba73fa05e2a2cb7703425a602f3a57
Ilya Makovich <makovich.i@mail.ru>
2023-11-17 21:21:38 +0300

    feature(swagger) add enum docs

--- CabWebApi/Content/Attributes/RequiredAgeAttribute.cs
+++ CabWebApi/Content/Attributes/RequiredAgeAttribute.cs
@@ -5,23 +5,23 @@ namespace CabWebApi.Content.Attributes;
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public class RequiredAgeAttribute : ValidationAttribute
 {
-    private double bottomDaysBorder;
-    private double topDaysBorder;
-    public RequiredAgeAttribute(byte bottomAgeBorder, byte topAgeBorder)
-    {
-        bottomDaysBorder = bottomAgeBorder * 365.2425;
-        topDaysBorder = topAgeBorder * 365.2425;
-    }
-    public override bool IsValid(object? value)
-    {
-        if (value != null && value is DateTime)
-        {
-            DateTime date = (DateTime)value;
-            TimeSpan age = DateTime.Now - date;
-            if (age > TimeSpan.FromDays(bottomDaysBorder) &&
-                age < TimeSpan.FromDays(topDaysBorder))
-                return true;
-        }
-        return false;
-    }
+	private double bottomDaysBorder;
+	private double topDaysBorder;
+	public RequiredAgeAttribute(byte bottomAgeBorder, byte topAgeBorder)
+	{
+		bottomDaysBorder = bottomAgeBorder * 365.2425;
+		topDaysBorder = topAgeBorder * 365.2425;
+	}
+	public override bool IsValid(object? value)
+	{
+		if (value != null && value is DateTime)
+		{
+			DateTime date = (DateTime)value;
+			TimeSpan age = DateTime.Now - date;
+			if (age > TimeSpan.FromDays(bottomDaysBorder) &&
+				age < TimeSpan.FromDays(topDaysBorder))
+				return true;
+		}
+		return false;
+	}
 }
---
+++ CabWebApi/Content/Swagger/EnumSchemaFilter.cs
@@ -0,0 +1,28 @@
+﻿using CabWebApi.Domain.Core;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace CabWebApi.Content.Swagger;
+public class EnumSchemaFilter : ISchemaFilter
+{
+	public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+	{
+		if (context.Type.IsEnum)
+		{
+			StringBuilder builder = new("<p>Contains enum values</p><ul>");
+			foreach (var enumMember in Enum.GetValues(context.Type))
+			{
+				string? memberName = Enum.GetName(context.Type, enumMember);
+				int memberValue = Convert.ToInt32(enumMember);
+				builder.Append($"<li>{memberValue} - {memberName}</li>");
+			}
+			builder.Append("</ul>");
+			schema.Description += builder.ToString();
+		}
+	}
+}
