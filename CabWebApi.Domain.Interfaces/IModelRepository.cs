8076cd49f520f2d0a1ee0ed2e19e83bcb7a0ebe7
Ilya Makovich <makovich.i@mail.ru>
2023-11-17 14:31:45 +0300

    refactor(model services) change GetAllWithAsync method add override

--- CabWebApi.Domain.Interfaces/IModelRepository.cs
+++ CabWebApi.Domain.Interfaces/IModelRepository.cs
@@ -2,14 +2,14 @@
 
 namespace CabWebApi.Domain.Interfaces;
 public interface IModelRepository<TModel>
-    where TModel : class
+	where TModel : class
 {
-    Task<List<TModel>> GetAllAsync();
-    Task<List<TModel>> GetAllWithAsync(string propertyName, params object?[] propertyValues);
-    ValueTask<TModel?> GetAsync(int id);
-    ValueTask<EntityEntry<TModel>> CreateAsync(TModel model);
-    // изменить апдейт
-    void Update(TModel model);
-    void Delete(TModel model);
-    Task<int> SaveChangesAsync();
+	Task<List<TModel>> GetAllAsync();
+	Task<List<TModel>> GetAllWithAsync(string propertyName, object? propertyValue);
+	Task<List<TModel>> GetAllWithAsync(string propertyName, IEnumerable<object> propertyValues);
+	ValueTask<TModel?> GetAsync(int id);
+	ValueTask<EntityEntry<TModel>> CreateAsync(TModel model);
+	void Update(TModel model);
+	void Delete(TModel model);
+	Task<int> SaveChangesAsync();
 }
--- CabWebApi.Infrastructure.Data/ModelRepository.cs
+++ CabWebApi.Infrastructure.Data/ModelRepository.cs
@@ -2,48 +2,69 @@
 using CabWebApi.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using CabWebApi.Domain.Core;
+using CabWebApi.Infrastructure.Data.Extensions;
 
 namespace CabWebApi.Infrastructure.Data;
 // решить проблему с асинхронностью, настроить реализацию
 public class ModelRepository<TModel> : IModelRepository<TModel>
-    where TModel : class
+	where TModel : class
 {
-    private readonly DbContext context;
-    public ModelRepository(DbContext context)
-    {
-        this.context = context;
-    }
+	private readonly DbContext context;
+	public ModelRepository(DbContext context)
+	{
+		this.context = context;
+	}
 
-    public ValueTask<EntityEntry<TModel>> CreateAsync(TModel model) =>
-        context.Set<TModel>().AddAsync(model);
+	public ValueTask<EntityEntry<TModel>> CreateAsync(TModel model) =>
+		context.Set<TModel>().AddAsync(model);
 
-    public void Delete(TModel model) =>
-        context.Set<TModel>().Remove(model);
+	public void Delete(TModel model) =>
+		context.Set<TModel>().Remove(model);
 
-    public Task<List<TModel>> GetAllAsync() =>
-        context.Set<TModel>().ToListAsync();
+	public Task<List<TModel>> GetAllAsync() =>
+		context.Set<TModel>().ToListAsync();
 
-    public Task<List<TModel>> GetAllWithAsync(string propertyName, params object?[] propertyValues) =>
-        context.Set<TModel>()
-               .Select(model => model)
-               .Where(new Func<TModel, bool>((model) =>
-               {
-                   var property = model.GetType()
-                                       .GetProperty(propertyName);
-                   foreach (var propertyValue in propertyValues)
-                       if (property?.GetValue(model) == propertyValue)
-                           return true;
-                   return false;
-               }))
-               .AsQueryable()
-               .ToListAsync();
+	public Task<List<TModel>> GetAllWithAsync(string propertyName, object? propertyValue)
+	{
+		return context.Set<TModel>()
+					  .Select(model => model)
+					  .Where(new Func<TModel, bool>((model) =>
+					  {
+						  object? modelValue = model.GetType()
+													.GetProperty(propertyName)
+												   ?.GetValue(model);
+						  return modelValue is not null &&
+								 modelValue.Equals(propertyValue) ? true : false;
+					  }))
+					  .AsQueryable()
+					  .ToListSafeAsync();
+	}
+	public Task<List<TModel>> GetAllWithAsync(string propertyName, IEnumerable<object> propertyValues)
+	{
+		return context.Set<TModel>()
+					  .Select(model => model)
+					  .Where(new Func<TModel, bool>((model) =>
+					  {
+						  object? modelValue = model.GetType()
+													.GetProperty(propertyName)
+													?.GetValue(model);
+						  if (modelValue is null)
+							  return false;
+						  foreach (var propertyValue in propertyValues)
+							  if (modelValue.Equals(propertyValue))
+								  return true;
+						  return false;
+					  }))
+					  .AsQueryable()
+					  .ToListSafeAsync();
+	}
 
-    public ValueTask<TModel?> GetAsync(int id) =>
-        context.Set<TModel>().FindAsync(id);
+	public ValueTask<TModel?> GetAsync(int id) =>
+		context.Set<TModel>().FindAsync(id);
 
-    public Task<int> SaveChangesAsync() =>
-        context.SaveChangesAsync();
+	public Task<int> SaveChangesAsync() =>
+		context.SaveChangesAsync();
 
-    public void Update(TModel model) =>
-        context.Set<TModel>().Update(model);
+	public void Update(TModel model) =>
+		context.Set<TModel>().Update(model);
 }
