e57fc903257c7a16c51eacb3e29020e34a0f4ced
Ilya Makovich <makovich.i@mail.ru>
2023-11-17 20:23:43 +0300

    feature(context) add migration update packages add TPC and Person to context add ToListAsyncSafe() for data access

---
+++ CabWebApi.Infrastructure.Data/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CabWebApi.Infrastructure.Data.Extensions;
+public static class IQueryableExtensions
+{
+	public static Task<List<TModel>> ToListSafeAsync<TModel>(this IQueryable<TModel> query)
+	{
+		if (query is null)
+			throw new ArgumentException($"{nameof(IQueryable<TModel>)} is null");
+
+		if (query is not IAsyncEnumerable<TModel>)
+			return Task.FromResult(query.ToList());
+
+		return query.ToListAsync();
+	}
+}
